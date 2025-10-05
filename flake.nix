b0a4feefa0d8eafd540a9dbb40a0b3832cf8ff16
Axel <nigthmaresans2@gmail.com>
2025-10-05 17:37:51 -0600

    feat: now using flakes

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      nativeBuildInputs = with pkgs; [
+        meson
+        ninja
+        pkg-config
+        vala
+        wrapGAppsHook4
+        blueprint-compiler
+        gobject-introspection
+      ];
+
+      buildInputs = with pkgs; [
+        gtk4
+        libadwaita
+        glib
+        polkit
+        gtk4-layer-shell
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs;
+
+        packages = with pkgs; [
+          vala-language-server
+          uncrustify
+        ];
+      };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "kagent";
+        version = "0.1";
+        src = ./.;
+        inherit nativeBuildInputs buildInputs;
+      };
+    };
+}
